6e6bfbe042d5d2512b33cdf24b3814c3a26efc37
GordonLz <451813289@qq.com>
2019-12-24 11:40:53 +0800

    update columns.js

--- dva-admin/src/routes/ArticleList/components/columns.js
+++ dva-admin/src/routes/ArticleList/components/columns.js
@@ -4,74 +4,14 @@ import { Input } from 'antd';
 const { TextArea } = Input;
 export default (self) => [
   {
-    title: '文章标题',
-    name: 'title',
-    formItem: {
-      rules: [
-        {
-          required: true,
-          message: '文章标题'
-        }
-      ],
-      col: { xs: { span: "24" }, md: { span: "12" }, sm: { span: "24" }, lg: { span: "12" }, xl: { span: "8" }, xxl: { span: "8" } },
-    }
-  },
-  {
-    title: '文章类型',
-    name: 'articleType',
-    dict: [...self.article_type.map(item => {
-      return { code: item.id, codeName: item.typeName }
-    })],
-    formItem: {
-      type: 'select',
-      rules: [
-        {
-          required: true,
-          message: '文章类型'
-        }
-      ],
-      col: { xs: { span: "24" }, md: { span: "12" }, sm: { span: "24" }, lg: { span: "12" }, xl: { span: "8" }, xxl: { span: "8" } },
-    }
-  },
-  // {
-  //     title: '发布日期',
-  //     name: 'addTime',
-  //     formItem: {
-  //         type: 'datetime',
-  //         showTime: true,
-  //         initialValue: moment(),
-  //         col: { xs: { span: "24" }, md: { span: "12" }, sm: { span: "24" }, lg: { span: "12" }, xl: { span: "8" }, xxl: { span: "6" } },
-  //     }
-  // },
-
-  {
-    title: '排序',
-    name: 'sortNumber',
-    formItem: {
-      type: 'number',
-      col: { xs: { span: "24" }, md: { span: "12" }, sm: { span: "24" }, lg: { span: "12" }, xl: { span: "8" }, xxl: { span: "8" } },
-    }
-  },
-  {
-    title: '文章简介',
-    formItem: {
-      type: 'custom',
-      col: { xs: { span: "24" }, sm: { span: "24" }, md: { span: "24" }, lg: { span: "24" }, xl: { span: "24" }, xxl: { span: "24" } },
-      formItemLayout: {
-        labelCol: { span: 2 },
-        wrapperCol: { span: 12 }
-      },
-      render: (record, form) => {
-        const { getFieldDecorator } = form;
-        return (
-          <div>
-            {getFieldDecorator('introduce', {
-              initialValue: record && record.introduce
-            })(<TextArea rows={2} />)}
-          </div>
-        );
-      }
-    }
+    title: '标题',
+    name: 'article_title',
+    tableItem: {
+      width: 120,
+      fixed: 'left',
+    },
+    formItem: {},
+    searchItem: {}
   },
   {
     title: '操作',
--- dva-admin/src/routes/ArticleList/components/index.js
+++ dva-admin/src/routes/ArticleList/components/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Layout } from 'antd';
+import { Layout, Button } from 'antd';
 import BaseComponent from 'components/BaseComponent';
-const { Content } = Layout;
+import Toolbar from 'components/Toolbar';
+import SearchBar from 'components/SearchBar';
+import DataTable from 'components/DataTable';
+import { ModalForm } from 'components/Modal';
+import createColumns from './columns';
+import './index.less';
+const { Content, Header, Footer } = Layout;
+const Pagination = DataTable.Pagination;
 
 
 @connect(({ articleList, loading }) => ({
@@ -10,12 +17,48 @@ const { Content } = Layout;
   loading: loading.models.articleList
 }))
 export default class extends BaseComponent {
+
+  state = {
+    record: null,
+    visible: false,
+    rows: []
+  };
+
   render() {
-    const articleTypeList=this.props
-    console.log(articleTypeList)
+    const { articleList, loading, dispatch } = this.props;
+    const { pageData, article_type } = articleList;
+    const columns = createColumns(this);
+    const { rows, record, visible } = this.state;
+
+    console.log(pageData)
+
+    const dataTableProps = {
+      loading,
+      columns,
+      rowKey: 'article_id',
+     // isScroll: { x: 666, y: 500}, //固定表头和左右固定空白问题需要设一个宽 
+      dataItems: pageData,
+      selectType: 'checkbox',
+      showNum: true, //行号
+      selectedRowKeys: rows.map(item => item.rowKey),
+      onChange: ({ pageNum, pageSize }) => {
+        dispatch({
+          type: 'articleList/getPageInfo',
+          payload: {
+            pageData: pageData.jumpPage(pageNum, pageSize)
+          }
+        });
+      },
+      onSelect: (keys, rows) => this.setState({ rows })
+    };
+
+
+
     return (
-      <Layout className="full-layout page blank-page">
-        <Content>空白页</Content>
+      <Layout className="full-layout page articleList-page">
+       <Content>
+          <DataTable {...dataTableProps} />
+        </Content>
       </Layout>
     );
   }
