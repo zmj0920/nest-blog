365515ed8b6e9fe4864a396a0d36b5fd5aec4876
zmj0920 <mj960920@163.com>
2019-12-25 23:10:55 +0800

    update 添加文章md组件话

--- dva-admin/src/routes/Article/components/index.js
+++ dva-admin/src/routes/Article/components/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Editor from 'for-editor';
 import { connect } from 'dva';
-import { Layout, Button, message, Row, Col } from 'antd';
+import { Layout, Row, Col } from 'antd';
 import BaseComponent from 'components/BaseComponent';
 import Panel from 'components/Panel';
 import Form from 'components/Form';
@@ -22,9 +22,7 @@ export default class extends BaseComponent {
             mobile: null,
             user: $$.getStore('user')
         }
-        this.$vm = React.createRef()
-
-        this.$from=React.createRef()
+        this.$from = React.createRef()
     }
 
     resize() {
@@ -45,7 +43,11 @@ export default class extends BaseComponent {
             this.resize()
         })
     }
-
+    handleChange = value => {
+        this.setState({
+            value
+        })
+    }
 
     onSubmit = (values) => {
         const { dispatch } = this.props;
@@ -56,29 +58,53 @@ export default class extends BaseComponent {
                 article,
                 success: () => {
                     this.$from.current.resetFields()
-                    this.state.value=""
+                    this.state.value = ""
                 }
             }
         });
     }
-
-    handleChange(value) {
-        this.setState({
-            value
-        })
-    }
-
-    addImg($file) {
-        this.$vm.current.$img2Url($file.name, 'file_url')
-        console.log($file)
-    }
-
     render() {
         const { value } = this.state
         const { article } = this.props
         const columns = createColumns(article);
+        const mineditorProps = {
+            value: value,
+            height: 700,
+            placeholder: '文章内容',
+            onChange: this.handleChange,
+            toolbar: {
+                h1: true, // h1
+                h2: true, // h2
+                h3: true, // h3
+                h4: true, // h4
+                img: true, // 图片
+                preview: true, // 预览
+                undo: true, // 撤销
+                redo: true, // 重做
+            }
+        };
 
-
+        const maxeditorProps = {
+            value: value,
+            height: 500,
+            placeholder: '文章内容',
+            onChange: this.handleChange,
+            toolbar: {
+                h1: true, // h1
+                h2: true, // h2
+                h3: true, // h3
+                h4: true, // h4
+                img: true, // 图片
+                link: true, // 链接
+                code: true, // 代码块
+                preview: true, // 预览
+                expand: true, // 全屏
+                undo: true, // 撤销
+                redo: true, // 重做
+                // save: true, // 保存
+                subfield: true, // 单双栏模式
+            }
+        };
         return (
             <Layout className="full-layout page">
                 <Content>
@@ -90,51 +116,10 @@ export default class extends BaseComponent {
                         </Col>
                         <Col span={24}>
                             {this.state.mobile && (
-                                <Editor
-                                    ref={this.$vm}
-                                    height="500px"
-                                    toolbar={{
-                                        h1: true, // h1
-                                        h2: true, // h2
-                                        h3: true, // h3
-                                        h4: true, // h4
-                                        img: true, // 图片
-                                        preview: true, // 预览
-                                        /* v0.0.9 */
-                                        undo: true, // 撤销
-                                        redo: true, // 重做
-                                    }}
-                                    value={value}
-                                    subfield={false}
-                                    onChange={value => this.handleChange(value)}
-
-                                />
+                                <Editor {...mineditorProps} />
                             )}
                             {!this.state.mobile && (
-                                <Editor
-                                    ref={this.$vm}
-                                    height="700px"
-                                    toolbar={{
-                                        h1: true, // h1
-                                        h2: true, // h2
-                                        h3: true, // h3
-                                        h4: true, // h4
-                                        img: true, // 图片
-                                        link: true, // 链接
-                                        code: true, // 代码块
-                                        preview: true, // 预览
-                                        expand: true, // 全屏
-                                        /* v0.0.9 */
-                                        undo: true, // 撤销
-                                        redo: true, // 重做
-                                        // save: true, // 保存
-                                        /* v0.2.3 */
-                                        subfield: true, // 单双栏模式
-                                    }}
-                                    value={value}
-                                    addImg={($file) => this.addImg($file)}
-                                    onChange={value => this.handleChange(value)}
-                                />
+                                <Editor {...maxeditorProps} />
                             )}
                         </Col>
                     </Row>
